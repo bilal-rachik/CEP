685c856f1553ea64f3829464ef169bc2529486f3
pc-bilal <rachik.bilal@gmail.com>
2019-07-22 14:28:43 +0200

    =version 1

--- consumer.py
+++ consumer.py
@@ -1,31 +1,44 @@
-from confluent_kafka import Consumer, KafkaError
-import logging
 from cassandra.cluster import Cluster
-
-logging.basicConfig(level=logging.DEBUG)
-
-c = Consumer({
-   'bootstrap.servers': '35.180.242.51:9092',
-   'group.id': 'mygroup',
-   'auto.offset.reset': 'earliest'
-})
-
-cluster = Cluster(['35.180.242.51'])
+from streamz import Stream
+import json
+from dask.distributed import Client
+from time import sleep
+import random
+client = Client('35.180.242.51:8786')
+cluster = Cluster(['35.180.227.49'])
 session = cluster.connect('supramoteur')
-
-c.subscribe(['supramoteur'])
-
-while True:
-   msg = c.poll(1.0)
-   if msg is None:
-       continue
-   if msg.error():
-       print("Consumer error: {}".format(msg.error()))
-       continue
-
-   print('Received message: {}'.format(msg.value().decode('utf-8')))
-   rows = session.execute('SELECT * FROM test')
-   for user_row in rows:
-       print('user ==> ', user_row)
-
-c.close()
+source = Stream.from_kafka(['supramoteur'], {
+  'bootstrap.servers': '35.180.242.51:9092',
+  'group.id': 'mygroup'
+}, loop=client.loop)
+def save_data(entry):
+   session.execute("""
+   INSERT INTO client2 ( clientid,nametype,create_date, category,enddate,label,name,startdate,status,subject)
+   VALUES (%s, %s, now(), %s, %s, %s, %s, %s, %s, %s)
+   """,        (str(entry['clientId']),
+                str(entry['nametype']),
+                str(entry['category']),
+                str(entry['endDate']),
+                str(entry['label']),
+                str(entry['name']),
+                str(entry['startDate']),
+                str(entry['status']),
+                str(entry['subject'])
+                ))
+def print_me(x):
+   print(f"Result => {x}")
+def aggregate(acc, df):
+   return acc+1
+futures = source\
+   .scatter()\
+   .map(json.loads)\
+   .gather()\
+   .sink(save_data)
+def do_poll():
+   msg = source.consumer.poll(0.1)
+   if msg and msg.value() and msg.error() is None:
+       return msg.value()
+print('source.loop', source.loop)
+while True :
+   sleep(random.random() / 10)
+   source.start()
--- producer.py
+++ producer.py
@@ -1,29 +1,27 @@
 from confluent_kafka import Producer
 import json
 import logging
+import pandas as pd
 logging.basicConfig(level=logging.DEBUG)
-
-
 p = Producer({'bootstrap.servers': '35.180.242.51:9092'})
-
+file = pd.read_csv("data/small_events.csv", sep=';')
+file['nametype'] = "event"
+listEvents = json.loads(file.to_json(orient='records'))
 def delivery_report(err, msg):
-   """ Called once for each message produced to indicate delivery result.
-       Triggered by poll() or flush(). """
-   if err is not None:
-       print('Message delivery failed: {}'.format(err))
-   else:
-       print('Message delivered to "{}" [{}]'.format(msg.topic(), msg.partition()))
-
-for data in [{'client_id': 122, 'event_type': 'REMISE DE CHEQUE'}]:
-   # Trigger any available delivery report callbacks from previous produce() calls
-   p.poll(0)
-
-   # Asynchronously produce a message, the delivery report callback
-   # will be triggered from poll() above, or flush() below, when the message has
-   # been successfully delivered or failed permanently.
-   print(f'sending {json.dumps(data)}')
-   p.produce('supramoteur', json.dumps(data).encode('utf-8'), callback=delivery_report)
-
+  """ Called once for each message produced to indicate delivery result.
+      Triggered by poll() or flush(). """
+  if err is not None:
+      print('Message delivery failed: {}'.format(err))
+  else:
+      print('Message delivered to "{}" [{}]'.format(msg.topic(), msg.partition()))
+for data in listEvents:
+  # Trigger any available delivery report callbacks from previous produce() calls
+  p.poll(0)
+  # Asynchronously produce a message, the delivery report callback
+  # will be triggered from poll() above, or flush() below, when the message has
+  # been successfully delivered or failed permanently.
+  print(f'sending {json.dumps(data)}')
+  p.produce('supramoteur', json.dumps(data).encode('utf-8'), callback=delivery_report)
 # Wait for any outstanding messages to be delivered and delivery report
 # callbacks to be triggered.
 p.flush()
